590aa035783a12a4b7ce52d02af4e6143de2e9c3
VladislavTroinin <>
2021-07-23 23:36:05 +0300

    Create the empty project.

---
+++ src/main/java/com/practice/rosreestr/RosreestrApplication.java
@@ -0,0 +1,13 @@
+package com.practice.rosreestr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RosreestrApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RosreestrApplication.class, args);
+    }
+
+}
